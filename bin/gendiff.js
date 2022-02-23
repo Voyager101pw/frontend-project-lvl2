cbe3859ffa8f8476be70f1ea0358894912658275
Voyager101pw <voyager101sirius@gmail.com>
2022-02-24 00:57:00 +0500

    Step 2: Added two arguments and add one option in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>');
 
 program.parse();
