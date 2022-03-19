b7be74420f845decb570a1ce269f4870d7dc55f4
Voyager101pw <voyager101sirius@gmail.com>
2022-03-20 03:55:51 +0500

    Step 7: Added formatter - Plain

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,10 +5,11 @@ import genDiff from '../src/index.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .option('-f, --formatter <type>', 'output format', 'stylish')
+  .option('-f, --formatName <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((path1, path2, { formatter }) => {
-    console.log(genDiff(path1, path2, formatter));
+  .action((path1, path2, { formatName }) => {
+    const diff = genDiff(path1, path2, formatName);
+    console.log(diff);
   });
 
 program.parse();
--- src/formatters/plainRender.js
+++ src/formatters/plainRender.js
@@ -1,1 +1,30 @@
-export default {};
+import _ from 'lodash';
+
+const getValue = (value) => {
+  if (_.isObject(value)) return '[complex value]';
+  return typeof value === 'string' ? `'${value}'` : value;
+};
+
+const renderMessage = (array, res = []) => {
+  const rec = (arr) => arr.forEach((i) => (Array.isArray(i) ? rec(i) : res.push(i)));
+  rec(array);
+  return res.join('\n');
+};
+
+const genMessage = (tree, path = []) => {
+  if (tree.status) {
+    const pathToProp = [...path, tree.name].join('.');
+    const operations = {
+      added: () => `Property '${pathToProp}' was added with value: ${getValue(tree.value)}`,
+      deleted: () => `Property '${pathToProp}' was removed`,
+      update: () => `Property '${pathToProp}' was updated. From ${getValue(tree.oldValue)} to ${getValue(tree.newValue)}`,
+      saved: () => [],
+      hasChildren: () => genMessage(tree.children, [...path, tree.name]),
+    };
+    return operations[tree.status]();
+  }
+  const res = tree.map((node) => genMessage(node, path));
+  return renderMessage(res);
+};
+
+export default genMessage;
